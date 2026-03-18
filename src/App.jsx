8005fbeabc268731f69eb4871ff178cc8423d573
Sergio Gonzalez <swgonzalez@unbosque.edu.co>
2026-03-17 23:44:15 -0500

    quotes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import {
 import { getRuntimeEnv } from "./runtimeConfig";
 
 const LOGIN_URL = getRuntimeEnv("VITE_LOGIN_URL", "/login");
-LOGIN_URL.replace(/"/g, "");
+LOGIN_URL.replaceAll('"', '');
 console.log("LOGIN_URL:", LOGIN_URL);
 const WHATSAPP_URL = getRuntimeEnv(
   "VITE_WHATSAPP_URL",
